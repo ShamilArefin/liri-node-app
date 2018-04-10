6346a4f43dc1d56131ea70d0d239f09a5eccda5a
Shamil Arefin <landoflitany@gmail.com>
2018-04-10 13:36:45 -0500

    uWu Commit

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const inquirer = require("inquirer");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+const request = require("request");
+
+
